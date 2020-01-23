b7e00e64662b867a084ec18729235f714cd773aa
Lucasraico <45289975+Lucasraico@users.noreply.github.com>
2020-01-23 09:42:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import socket
 import struct
 import time
 
-
 def decode(msg):
-     
     mac_dst = struct.unpack('!BBBBBB', msg[0:6])
     mac_dst = ":".join([hex(part)[2:].zfill(2) for part in mac_dst])
     mac_src = struct.unpack('!BBBBBB', msg[6:12])
@@ -12,40 +10,44 @@ def decode(msg):
     ether_type = struct.unpack('!H', msg[12:14])
     ether_type = ":".join([hex(part)[2:].zfill(4) for part in ether_type])
 
-
-    portSource = struct.unpack('!H', msg[34:36])[0]
-    portDestination = struct.unpack('!H', msg[36:38])[0]
-    msgAll = msg[54:].decode(errors='ignore')    
-  
     if ether_type == '0800':
-        print('ok1')
-        print(f"ETHER_TYPE: {ether_type} - ")
-        if portDestination == '80':
-            f"port destination: {portDestination} - "
-            print('ok2')
-    
-    print(
-    #f"{time.time()}: DST_MAC: {mac_dst} - "
-    #f"SRC_MAC: {mac_src} - "
-    
-    #f"port : {portSource} - "
-    
-    f"msg est {msgAll}"
+        enteteip = struct.unpack('!B', msg[23:24])
+        enteteip = ":".join([hex(part)[2:].zfill(4) for part in enteteip])
 
-    )
+        if enteteip == '0006':
+            port_source = struct.unpack('!H', msg[34:36])[0]
+            port_dest = struct.unpack('!H', msg[36:38])[0]
+            offset = struct.unpack('!H', msg[46:48])
+            offset = offset[0]
+            offset = bin(offset)[2:].zfill(16)[:4]
+            offset = int(offset, base=2)
+            offset = 34 + (offset * 4)
+
+            if port_dest == 80 or port_source == 80:
+                http_request = msg[offset:].decode(errors='ignore')
+                                          
+                print(
+                f"{time.time()}: DST_MAC: {mac_dst} - "
+                f"SRC_MAC: {mac_src} - "
+                f"ETHER_TYPE: {ether_type} - "
+                f"ENTETE_IP: {enteteip} - "
+                f"PORT_SOURCE: {port_source} - "
+                f"PORT_DEST: {port_dest} - "
+                f"OFFSET: {offset} - "
+                f"HTTP_REQUEST: {http_request}"
+                )
 
 
-    
-   
-    
+
 def main():
     s = socket.socket(
             socket.AF_PACKET,
             socket.SOCK_RAW,
             socket.htons(3),
-    )    
-    
+    )
+
     s.bind(("enp0s3", 3))
+
     try:
         print("Sniffer started")
         while True:
@@ -53,7 +55,7 @@ def main():
             decode(msg)
     except KeyboardInterrupt:
         print("Sniffer stopped")
-        
-    
+
+
 if __name__ == "__main__":
     main()
